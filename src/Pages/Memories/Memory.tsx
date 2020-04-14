15005ef8e9a5f5007f768fd218b0d444e22a247c
lucaspelloni2 <lucas_pelloni@hotmail.com>
2020-04-14 21:59:06 +0200

    Update Memory.tsx

--- src/Pages/Memories/Memory.tsx
+++ src/Pages/Memories/Memory.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { PageContainer } from "../../Layout/styled/PageContainer";
 import { FlexBox } from "../../Layout/styled/FlexBox";
 import { MEMORY_LEFT_PANEL_WIDTH } from "../../Layout/Theme";
+import { Memories } from "../../Content";
 
 const Container = styled(PageContainer)`
   display: flex;
@@ -11,17 +12,31 @@ const Container = styled(PageContainer)`
   padding: 0;
 `;
 
-const Content = styled.div`
+const Img = styled.div<{ url: string }>`
+  background: ${props => `url(${props.url}) no-repeat center center fixed`};
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  width: 100%;
   height: 100%;
+`;
+
+const Content = styled.div`
+  height: 100vh;
   width: 100%;
   margin-left: ${MEMORY_LEFT_PANEL_WIDTH}px;
 `;
 
 export const Memory = () => {
+  // reduxify this
+  const currentMemory = Memories[0];
   return (
     <Container>
       <FlexBox flex={1}>
-        <Content>This is amesome conten</Content>
+        <Content>
+          <Img url={currentMemory.achievement.pictures[0].src} />
+        </Content>
       </FlexBox>
     </Container>
   );
