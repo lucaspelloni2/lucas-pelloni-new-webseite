939a6ae31268a7cc131f0b5b19f24dfacba36a87
lucaspelloni2 <lucas_pelloni@hotmail.com>
2020-12-08 20:44:38 +0100

    Update AnimationHelper.tsx

--- src/Layout/AnimationHelper.tsx
+++ src/Layout/AnimationHelper.tsx
@@ -43,3 +43,16 @@ export const slideLeft = keyframes`
     transform: translateX(-${SPACING * 8}px);
   }
 `;
+
+export const textFocusIn = keyframes`
+   0% {
+    -webkit-filter: blur(12px);
+            filter: blur(12px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-filter: blur(0px);
+            filter: blur(0px);
+    opacity: 1;
+  }
+`;
