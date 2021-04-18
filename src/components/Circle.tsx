fe424e256663f5addf36153525b1b8b33993d7a2
lucaspelloni2 <lucas_pelloni@hotmail.com>
2021-04-18 20:55:38 +0200

    Update Circle.tsx

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -3,11 +3,8 @@ import styled from "styled-components";
 import {useNormalizedTransition} from "../hooks/useNormalizedTransition";
 import {useTheme} from "../hooks/useTheme";
 import {useWindowSize} from "../hooks/useWindowSize";
-import {MEDIUM_DEVICES} from "../Layout/Mobile";
 import {
   AnimatedOpacityContainer,
-  CIRCLE_RIGHT_OVERFLOW,
-  CIRCLE_TOP_OVERFLOW,
   PageDimensions,
   PAGE_TRANSITION
 } from "../Layout/Theme";
@@ -19,78 +16,63 @@ type CircleProps = {
 };
 
 const CircleContainer = styled(AnimatedOpacityContainer).attrs<{
-  left: number;
   visible: boolean;
-}>(({right, translation}: CircleProps) => ({
+}>(({translation}: CircleProps) => ({
   style: {
-    transform: `translate(${right - CIRCLE_RIGHT_OVERFLOW}vw, ${translation}vh)`
+    transform: `translate(50%, ${translation}vh)`
   }
-}))`
+}))<{visible: boolean; translation: number}>`
   position: fixed;
   z-index: -1;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  bottom: 0;
   transition: ${PAGE_TRANSITION};
-  top: -${CIRCLE_TOP_OVERFLOW}px;
-  ${MEDIUM_DEVICES`
-      left: -150px;
-  `}
+  top: 0;
 `;
 
 const MyCircle = styled.div<{
   color: string;
   size: number;
-  scale: number;
 }>`
   border-radius: 50%;
-  width: 50vw;
-  height: 50vw;
-  transform: scale(${props => props.scale});
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  transform: scale(1);
   transition: ${PAGE_TRANSITION};
   will-change: transform;
   background: ${props => props.color};
-  ${MEDIUM_DEVICES`
-      width: 500px;
-      height: 500px;
-  `}
 `;
 type Props = {
   visible: boolean;
 };
 export const Circle = ({visible}: Props) => {
+  const {height, width} = useWindowSize();
   const {selectedColor} = useAppState(s => s.selectedColor);
   const {background} = useTheme();
   const {translation} = useNormalizedTransition();
-  const {width} = useWindowSize();
-  const [right, setRight] = useState(80);
-  const INITIAL_SIZE = 800;
   const [circleColor, setCircleColor] = useState(selectedColor);
-  const [scale, setScale] = useState(1);
+
+  const circleSize = Math.min(height, width);
 
   useEffect(() => {
     if (translation === PageDimensions[0]) {
-      setRight(80);
       setCircleColor(selectedColor);
     } else if (translation === PageDimensions[1]) {
-      setRight(0);
-      setScale(1);
       setCircleColor(selectedColor);
     } else if (translation === PageDimensions[2]) {
-      setScale(3.5);
-      setRight(CIRCLE_RIGHT_OVERFLOW);
       setCircleColor(selectedColor);
     }
   }, [background, selectedColor, translation, width]);
 
   return useMemo(
     () => (
-      <CircleContainer
-        // @ts-ignore
-        right={right}
-        visible={visible}
-        translation={translation}
-      >
-        <MyCircle color={circleColor} size={INITIAL_SIZE} scale={scale} />
+      <CircleContainer visible={visible} translation={translation}>
+        <MyCircle color={circleColor} size={circleSize} />
       </CircleContainer>
     ),
-    [circleColor, right, scale, translation, visible]
+    [circleColor, circleSize, translation, visible]
   );
 };
